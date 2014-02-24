ab339c2043d3bc3f6effa27cecff9430948909da
Gabe <gabe@paperlesspost.com>
2014-02-24 16:57:45 -0500

    select Quicksort pivot more intelligently

--- sorting/quicksort.rb
+++ sorting/quicksort.rb
@@ -1,7 +1,7 @@
 class Quicksort
 
   def self.sort(list, from=0, to=list.size-1)
-    return list if to - from < 2
+    return list if to - from < 1
     pivot_index = partition(list, from, to)
     sort(list, from, pivot_index-1)
     sort(list, pivot_index+1, to)
@@ -9,8 +9,8 @@ class Quicksort
   end
 
   def self.partition(list, from, to)
-    pivot_index = from # naïve!
-    pivot_value = list[from]
+    pivot_index = pivot(list, from, to)
+    pivot_value = list[pivot_index]
     swap(list, pivot_index, to)
 
     cursor = from
@@ -22,6 +22,11 @@ class Quicksort
     end
     swap(list, cursor, to)
     cursor
+  end
+
+  def self.pivot(list, from, to)
+    samples = [from, from + (to-from)/2, to]
+    samples.sort_by{|i| list[i]}[1] # use median of the three samples
   end
 
   def self.swap(list, i, j)
